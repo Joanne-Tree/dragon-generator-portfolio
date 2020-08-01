475fc3d0c0a6473105753cc85e7022f8dcdcc3ac
unknown <tree.joanne@gmail.com>
2020-08-01 08:51:44 +0100

    created dragon variables?

---
+++ src/dragonVariables.js
@@ -0,0 +1,14 @@
+
+var dragonBodySize = ("small", "medium", "big");
+var dragonBodyShape = ("rounded", "noodle", "sharp");
+var dragonBodyColour = ("black", "blue", "green", "brown", "grey", "red");
+var dragonBodySkinType = ("smooth", "scaly", "feathered", "furry");
+var dragonBodyHeadNumber = (1, 2, 3, 4, 5);
+var dragonHornType = ("hornless", "curlyHorns", "straightHorns");
+var dragonHornNumber = (0, 1, 2, 3, 4, 5, 6);
+var dragonWingsNumber = (0, 2, 4, 6, 8);
+var dragonTailLength = ("none", "stubby", "short", "medium", "long");
+var dragonLegs = ("none", "2", "4");
+var dragonDiet = ("herbivore", "omnivore", "carnivore");
+var dragonFightTactic = ("hide", "run", "bite", "scratch", "breatheFire", "roar", "stretchWings", "dustCloud");
+var dragonSocial = ("loner", "familial", "all the dragons");
