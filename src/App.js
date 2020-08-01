6c7350f9174be02913a73dad204dc2d3d624d01c
unknown <tree.joanne@gmail.com>
2020-08-01 12:36:24 +0100

    dragon type

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 class Dragon {
 	constructor(){
-		this.dragon = "A dragon"
+		this.dragon = {
+			type: "A dragon"
+		}
 	}
+	// to do...make function that returns the dragon object
 }
 
 function createDragon(){
 	const newDragon = new Dragon();
+	// to do... call the function that returns a dragon object, then assign value returned from function
+	// to a new value
 	console.log(newDragon.dragon);
-	document.getElementById("root").innerHTML = newDragon.dragon;
+	// update the newDragon.dragon to this new variable
+	document.getElementById("root").innerHTML = newDragon.dragon.type;
+
+	// Bonus points: function that will format dragon object into a descriptive string of text.
+	// eg. "This dragon is small with a rounded body." etc
 }
 
 
