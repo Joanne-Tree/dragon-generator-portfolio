58bab4244b6d4943d45f1cb56d91d8c3ac9bb76c
unknown <tree.joanne@gmail.com>
2020-08-06 13:02:27 +0100

    Confusion

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ class Dragon {
 		}
 	}
 	// to do...make function that returns the dragon object
+	function returnDragonObject(dragonBodySize, dragonBodyShape, dragonBodyColour, 
+		dragonBodySkinType, dragonBodyHeadNumber, dragonHornType, dragonHornNumber, dragonWingsNumber, dragonTailLength, 
+		dragonLegs, dragonDiet, dragonFightTactic, dragonSocial){
+		const dragonObject = {
+			dragonBodySize: ["small", "medium", "big"],
+			dragonBodyShape: ["rounded", "noodle", "sharp"],
+			dragonBodyColour: ["black", "blue", "green", "brown", "grey", "red"],
+			dragonBodySkinType: ["smooth", "scaly", "feathered", "furry"],
+			dragonBodyHeadNumber: [1, 2, 3, 4, 5],
+			dragonHornType: ["hornless", "curlyHorns", "straightHorns"],
+			dragonHornNumber: [0, 1, 2, 3, 4, 5, 6],
+			dragonWingsNumber: [0, 2, 4, 6, 8],
+			dragonLegs: ["none", "2", "4"],
+			dragonDiet: ["herbivore", "omnivore", "carnivore"],
+			dragonFightTactic: ["hide", "run", "bite", "scratch", "breatheFire",
+ "roar", "stretchWings", "dustCloud"],
+			dragonSocial: ["loner", "familial", "all the dragons"]
+		};
+		return dragonObject;
+		
+	}
 }
 
 function createDragon(){
