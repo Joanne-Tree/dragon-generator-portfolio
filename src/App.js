1f33abc4f5d22d29328f5a322d867bc3f6761724
unknown <tree.joanne@gmail.com>
2020-08-06 14:41:41 +0100

    object function bug

--- src/App.js
+++ src/App.js
@@ -2,41 +2,28 @@ class Dragon {
 	constructor(){
 		this.dragon = {
 			type: "A dragon"
-			dragonBodySize: ["small", "medium", "big"],
-			dragonBodyShape: ["rounded", "noodle", "sharp"],
-			dragonBodyColour: ["black", "blue", "green", "brown", "grey", "red"],
-			dragonBodySkinType: ["smooth", "scaly", "feathered", "furry"],
-			dragonBodyHeadNumber: [1, 2, 3, 4, 5],
-			dragonHornType: ["hornless", "curlyHorns", "straightHorns"],
-			dragonHornNumber: [0, 1, 2, 3, 4, 5, 6],
-			dragonWingsNumber: [0, 2, 4, 6, 8],
-			dragonLegs: ["none", "2", "4"],
-			dragonDiet: ["herbivore", "omnivore", "carnivore"],
-			dragonFightTactic: ["hide", "run", "bite", "scratch", "breatheFire",
- "roar", "stretchWings", "dustCloud"],
-			dragonSocial: ["loner", "familial", "all the dragons"]
-
 		}
-	}
+}
 	// to do...make function that returns the dragon object
-	function returnDragonObject(dragonBodySize, dragonBodyShape, dragonBodyColour, 
-		dragonBodySkinType, dragonBodyHeadNumber, dragonHornType, dragonHornNumber, dragonWingsNumber, dragonTailLength, 
+	function returnDragonObject(dragonBodySize, dragonBodyShape, dragonBodyColour, dragonBodySkinType, 
+		dragonBodyHeadNumber, dragonHornType, dragonHornNumber, dragonWingsNumber, dragonTailLength, 
 		dragonLegs, dragonDiet, dragonFightTactic, dragonSocial){
 		const dragonObject = {
-			dragonBodySize: dragonBodySize,
-			dragonBodyShape: dragonBodyShape,
-			dragonBodyColour: dragonBodyColour,
-			dragonBodySkinType: dragonBodySkinType,
-			dragonBodyHeadNumber: dragonBodyHeadNumber,
-			dragonHornType: dragonHornType,
-			dragonHornNumber: dragonHornNumber,
-			dragonWingsNumber: dragonWingsNumber,
-			dragonLegs: dragonLegs,
-			dragonDiet: dragonDiet,
-			dragonFightTactic: dragonFightTactic,
-			dragonSocial: dragonSocial
+			dragonBodySize: "",
+			dragonBodyShape: "",
+			dragonBodyColour: "",
+			dragonBodySkinType: "",
+			dragonBodyHeadNumber: "",
+			dragonHornType: "",
+			dragonHornNumber: "",
+			dragonWingsNumber: "",
+			dragonLegs: "",
+			dragonDiet: "",
+			dragonFightTactic: "",
+			dragonSocial: ""
+
 		};
-		return dragonObject;
+		return dragonObject();
 		
 	}
 }
@@ -46,7 +33,7 @@ function createDragon(){
 	// to do... call the function that returns a dragon object, then assign value returned from function
 	// to a new value
 	returnDragonObject(dragonObject);
-	const dragon1 = dragonObject;
+	var dragon1 = dragonObject;
 	console.log(newDragon.dragon);
 	// update the newDragon.dragon to this new variable
 	newDragon.dragon = dragon1;
---
+++ src/scratch.js
@@ -0,0 +1,13 @@
+dragonBodySize.pickRandom(["small", "medium", "big"]),
+dragonBodyShape.pickRandom(["rounded", "noodle", "sharp"]),
+dragonBodyColour.pickRandom(["black", "blue", "green", "brown", "grey", "red"]),
+dragonBodySkinType.pickRandom(["smooth", "scaly", "feathered", "furry"]),
+dragonBodyHeadNumber.pickRandom([1, 2, 3, 4, 5]),
+dragonHornType.pickRandom(["hornless", "curlyHorns", "straightHorns"]),
+dragonHornNumber.pickRandom([0, 1, 2, 3, 4, 5, 6]),
+dragonWingsNumber.pickRandom([0, 2, 4, 6, 8]),
+dragonLegs.pickRandom(["none", "2", "4"]),
+dragonDiet.pickRandom(["herbivore", "omnivore", "carnivore"]),
+dragonFightTactic.pickRandom(["hide", "run", "bite", "scratch", "breatheFire",
+ "roar", "stretchWings", "dustCloud"]),
+dragonSocial.pickRandom(["loner", "familial", "all the dragons"])
