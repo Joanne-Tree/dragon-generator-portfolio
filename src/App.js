bad8df074a0f2adf6b7fbccefe8f496298242d7e
unknown <tree.joanne@gmail.com>
2020-08-06 13:09:41 +0100

    major confusion

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ class Dragon {
 	constructor(){
 		this.dragon = {
 			type: "A dragon"
-		}
-	}
-	// to do...make function that returns the dragon object
-	function returnDragonObject(dragonBodySize, dragonBodyShape, dragonBodyColour, 
-		dragonBodySkinType, dragonBodyHeadNumber, dragonHornType, dragonHornNumber, dragonWingsNumber, dragonTailLength, 
-		dragonLegs, dragonDiet, dragonFightTactic, dragonSocial){
-		const dragonObject = {
 			dragonBodySize: ["small", "medium", "big"],
 			dragonBodyShape: ["rounded", "noodle", "sharp"],
 			dragonBodyColour: ["black", "blue", "green", "brown", "grey", "red"],
@@ -22,6 +15,26 @@ class Dragon {
 			dragonFightTactic: ["hide", "run", "bite", "scratch", "breatheFire",
  "roar", "stretchWings", "dustCloud"],
 			dragonSocial: ["loner", "familial", "all the dragons"]
+
+		}
+	}
+	// to do...make function that returns the dragon object
+	function returnDragonObject(dragonBodySize, dragonBodyShape, dragonBodyColour, 
+		dragonBodySkinType, dragonBodyHeadNumber, dragonHornType, dragonHornNumber, dragonWingsNumber, dragonTailLength, 
+		dragonLegs, dragonDiet, dragonFightTactic, dragonSocial){
+		const dragonObject = {
+			dragonBodySize: dragonBodySize,
+			dragonBodyShape: dragonBodyShape,
+			dragonBodyColour: dragonBodyColour,
+			dragonBodySkinType: dragonBodySkinType,
+			dragonBodyHeadNumber: dragonBodyHeadNumber,
+			dragonHornType: dragonHornType,
+			dragonHornNumber: dragonHornNumber,
+			dragonWingsNumber: dragonWingsNumber,
+			dragonLegs: dragonLegs,
+			dragonDiet: dragonDiet,
+			dragonFightTactic: dragonFightTactic,
+			dragonSocial: dragonSocial
 		};
 		return dragonObject;
 		
@@ -32,8 +45,11 @@ function createDragon(){
 	const newDragon = new Dragon();
 	// to do... call the function that returns a dragon object, then assign value returned from function
 	// to a new value
+	returnDragonObject(dragonObject);
+	const dragon1 = dragonObject;
 	console.log(newDragon.dragon);
 	// update the newDragon.dragon to this new variable
+	newDragon.dragon = dragon1;
 	document.getElementById("root").innerHTML = newDragon.dragon.type;
 
 	// Bonus points: function that will format dragon object into a descriptive string of text.
