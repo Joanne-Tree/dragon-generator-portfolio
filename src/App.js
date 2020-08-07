2899364b6776ee5c6ddd3b5e15d5975e7485b3f5
unknown <tree.joanne@gmail.com>
2020-08-07 10:42:14 +0100

    made dragon object function

--- src/App.js
+++ src/App.js
@@ -5,27 +5,54 @@ class Dragon {
 		}
 }
 	// to do...make function that returns the dragon object
-	function returnDragonObject(dragonBodySize, dragonBodyShape, dragonBodyColour, dragonBodySkinType, 
+		const dragonObject = {
+			dragonBodySize: ["small", "medium", "big"],
+			dragonBodyShape: ["rounded", "noodle", "sharp"],
+			dragonBodyColour: ["black", "blue", "green", "brown", "grey", "red"],
+			dragonBodySkinType: ["smooth", "scaly", "feathered", "furry"],
+			dragonBodyHeadNumber: [1, 2, 3, 4, 5],
+			dragonHornType: ["hornless", "curlyHorns", "straightHorns"],
+			dragonHornNumber: [0, 1, 2, 3, 4, 5, 6],
+			dragonWingsNumber: [0, 2, 4, 6, 8],
+			dragonLegs: [0 , 2, 4],
+			dragonDiet: ["herbivore", "omnivore", "carnivore"],
+			dragonFightTactic: ["hide", "run", "bite", "scratch", "breatheFire",
+ "roar", "stretchWings", "dustCloud"],
+			dragonSocial: ["loner", "family group", "super social"],
+			makeDragon: function(dragonBodySize, dragonBodyShape, dragonBodyColour, dragonBodySkinType, 
 		dragonBodyHeadNumber, dragonHornType, dragonHornNumber, dragonWingsNumber, dragonTailLength, 
 		dragonLegs, dragonDiet, dragonFightTactic, dragonSocial){
-		const dragonObject = {
-			dragonBodySize: "",
-			dragonBodyShape: "",
-			dragonBodyColour: "",
-			dragonBodySkinType: "",
-			dragonBodyHeadNumber: "",
-			dragonHornType: "",
-			dragonHornNumber: "",
-			dragonWingsNumber: "",
-			dragonLegs: "",
-			dragonDiet: "",
-			dragonFightTactic: "",
-			dragonSocial: ""
+				dragonObject.dragonBodySize = dragonBodySize;
+				dragonObject.dragonBodyShape = dragonBodyShape;
+				dragonObject.dragonBodyColour = dragonBodyColour;
+				dragonObject.dragonBodySkinType = dragonBodySkinType;
+				dragonObject.dragonBodyHeadNumber = dragonBodyHeadNumber;
+				dragonObject.dragonHornType = dragonHornType;
+				dragonObject.dragonHornNumber = dragonHornNumber;
+				dragonObject.dragonWingsNumber = dragonWingsNumber;
+				dragonObject.dragonLegs = dragonLegs;
+				dragonObject.dragonDiet = dragonDiet;
+				dragonObject.dragonFightTactic = dragonFightTactic;
+				dragonObject.dragonSocial = dragonSocial;
 
-		};
-		return dragonObject();
-		
-	}
+				const myDragon = "Your dragon has a " + dragonObject.dragonBodySize + ", " +
+				dragonObject.dragonBodyShape + ", " + dragonObject.dragonBodyColour + ", " +
+				dragonObject.dragonBodySkinType + " body." + dragonObject.dragonBodyHeadNumber +
+				" heads. It has " + dragonObject.dragonHornNumber + " " + dragonObject.dragonHornType +
+				" horns, " + dragonObject.dragonWingsNumber + " wings, and " + dragonObject.dragonLegs +
+				" legs. It is a " + dragonObject.dragonDiet + ". Fight tactic: " + 
+				dragonObject.dragonFightTactic + ". Social status: " + dragonObject.dragonSocial + ".";
+				return myDragon;
+			}
+		}
+		const randomBodySize = Math.floor(Math.randomBodySize() * dragonObject.dragonBodySize.length);
+
+
+
+
+		const newDragon =
+		dragonObject.makeDragon("", "", "", "", "", "", "", "", "", "", "", "");
+		document.write (newDragon);
 }
 
 function createDragon(){
